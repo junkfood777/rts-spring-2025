e075c92b66ac46634167326d75cf9e9753ac7bcd
Andrey Gradusov <99825146+junkfood777@users.noreply.github.com>
2025-04-11 17:44:55 +0400

    Added solution for task01

--- tasks/task01/task01.ino
+++ tasks/task01/task01.ino
@@ -1,7 +1,34 @@
-void setup() {
+struct PinData {
+  uint8_t pin;           
+  uint32_t inter;      
+  volatile uint32_t time; 
+  bool state;             
+};
 
+PinData pins[] = {
+  {3,  10000, 0, LOW}, 
+  {5,   1000, 0, LOW}, 
+  {6,    500, 0, LOW}, 
+  {9,    100, 0, LOW}, 
+  {10,    50, 0, LOW}  
+};
+const uint8_t numPins = sizeof(pins)/sizeof(PinData);
+
+void setup() {
+  for (uint8_t i = 0; i < numPins; i++) {
+    pinMode(pins[i].pin, OUTPUT);
+  }
 }
 
 void loop() {
+  uint32_t currentTime = micros();
 
+  for (uint8_t i = 0; i < numPins; i++) {
+    if (currentTime - pins[i].time >= pins[i].inter) {
+      pins[i].time = currentTime;
+      
+      pins[i].state = !pins[i].state;
+      digitalWrite(pins[i].pin, pins[i].state);
+    }
+  }
 }
